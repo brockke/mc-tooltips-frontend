852229c6101b5b256aa11c5fc5d15d64674d45d8
brockke <brock97k@gmail.com>
2022-11-20 08:00:26 -0400

    Mouse hovering shows div

--- src/VideoOverlay.tsx
+++ src/VideoOverlay.tsx
@@ -1,5 +1,52 @@
+import useMouse from "@react-hook/mouse-position";
+import { useRef, useState } from "react";
+
 function VideoOverlay() {
-  return <div>Hello from Video Overlay!</div>;
+  const ref = useRef(null);
+  const mouse = useMouse(ref, {
+    enterDelay: 100,
+    leaveDelay: 100,
+  });
+
+  const [cursorPosition, setCursorPosition] = useState({ top: 0, left: 0 });
+  const onMouseMove = (e: any) =>
+    setCursorPosition({ top: e.screenY, left: e.screenX });
+  const [style, setStyle] = useState("invisible");
+
+  return (
+    <div>
+      <div className="h-auto w-auto">
+        <div className="absolute bottom-0 bg-red-500" onMouseMove={onMouseMove}>
+          <div className="flex" ref={ref}>
+            <div
+              onMouseEnter={() => {
+                setStyle("visible");
+              }}
+              onMouseLeave={() => {
+                setStyle("invisible");
+              }}
+            >
+              <h1>Test1</h1>
+            </div>
+            <div>
+              <h1>Test2</h1>
+            </div>
+            <div>
+              <h1>Test3</h1>
+            </div>
+            <div>
+              <h1>Test4</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div style={{ position: "absolute", ...cursorPosition }}>
+        <div className={style}>
+          <h1>MOVING</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default VideoOverlay;
