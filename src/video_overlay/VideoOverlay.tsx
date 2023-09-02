e8b4afd9c329a9e7f0646c4a9af0bab06cb63848
Brock Kennedy <brock97k@gmail.com>
2023-09-02 11:25:54 -0300

    Move video_overlay route to its own folder

---
+++ src/video_overlay/VideoOverlay.tsx
@@ -0,0 +1,45 @@
+function VideoOverlay() {
+  const gridItems = 36;
+  const gridRowLength = 9;
+  const inventoryData: { index: number; data: string }[] = Array.from(
+    { length: gridItems },
+    (_, i) => ({
+      index: i,
+      data: "",
+    })
+  );
+
+  // start listening to PubSub events
+  window.Twitch.ext.listen(
+    "broadcast",
+    (_target: string, contentType: string, message: string) => {
+      // verify content type
+      if (contentType !== "application/json") {
+        console.debug(`Unexpected contentType "${contentType}"`);
+        return;
+      }
+
+      // TODO: Put a type on this so only something
+      // that is the correct "shape will be parsed"
+      const jsonObj = JSON.parse(message);
+
+      console.log(jsonObj);
+    }
+  );
+
+  return (
+    <div className="grid h-screen place-items-center">
+      <div className={`grid grid-cols-${gridRowLength} gap-1`}>
+        {inventoryData.map((item) => (
+          <InventoryBox key={item.index} data={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const InventoryBox = (props: { data: { index: number; data: string } }) => {
+  return <div className="bg-rose-800 p-8">{props.data.index}</div>;
+};
+
+export default VideoOverlay;
---
+++ src/video_overlay/main.tsx
@@ -0,0 +1,19 @@
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import VideoOverlay from "../VideoOverlay";
+
+// Authorize extension
+window.Twitch.ext.onAuthorized((auth) => {
+  console.log("JWT", auth.token);
+  console.log("channelID", auth.channelId);
+});
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <VideoOverlay />
+  </React.StrictMode>
+);
